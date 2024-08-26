f73784bbcb0bb10bcfba97bc30da52c20f525440
Coed66 <tuanbkak66@gmail.com>
2024-08-26 16:53:10 +0700

    feat: check if mouth open in image

--- detect_open_mouth_image.py
+++ detect_open_mouth_image.py
@@ -34,8 +34,8 @@ def mouth_aspect_ratio(mouth):
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--shape-predictor", required=False, default='shape_predictor_68_face_landmarks.dat',
 	help="path to facial landmark predictor")
-ap.add_argument("-w", "--webcam", type=int, default=0,
-	help="index of webcam on system")
+ap.add_argument("-w", "--img-path", type=int, default=0,
+	help="path of image file")
 args = vars(ap.parse_args())
 
 # define one constants, for mouth aspect ratio to indicate open mouth
@@ -51,64 +51,34 @@ predictor = dlib.shape_predictor(args["shape_predictor"])
 (mStart, mEnd) = (49, 68)
 
 # start the video stream thread
-print("[INFO] starting video stream thread...")
-vs = VideoStream(src=args["webcam"]).start()
-time.sleep(1.0)
+frame = cv2.imread(src=args["img-path"])
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 frame_width = 640
 frame_height = 360
 
-# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 30, (frame_width,frame_height))
-time.sleep(1.0)
+rects = detector(gray, 0)
 
-# loop over frames from the video stream
-while True:
-	# grab the frame from the threaded video file stream, resize
-	# it, and convert it to grayscale
-	# channels)
-	frame = vs.read()
-	frame = imutils.resize(frame, width=640)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+is_talk_in_frame = False
+for rect in rects:
+	# determine the facial landmarks for the face region, then
+	# convert the facial landmark (x, y)-coordinates to a NumPy
+	# array
+	shape = predictor(gray, rect)
+	shape = face_utils.shape_to_np(shape)
 
-	# detect faces in the grayscale frame
-	rects = detector(gray, 0)
+	# extract the mouth coordinates, then use the
+	# coordinates to compute the mouth aspect ratio
+	mouth = shape[mStart:mEnd]
 
-	# loop over the face detections
-	for rect in rects:
-		# determine the facial landmarks for the face region, then
-		# convert the facial landmark (x, y)-coordinates to a NumPy
-		# array
-		shape = predictor(gray, rect)
-		shape = face_utils.shape_to_np(shape)
+	mouthMAR = mouth_aspect_ratio(mouth)
+	mar = mouthMAR
 
-		# extract the mouth coordinates, then use the
-		# coordinates to compute the mouth aspect ratio
-		mouth = shape[mStart:mEnd]
-
-		mouthMAR = mouth_aspect_ratio(mouth)
-		mar = mouthMAR
-		# compute the convex hull for the mouth, then
-		# visualize the mouth
-		mouthHull = cv2.convexHull(mouth)
-		
-		cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
-		cv2.putText(frame, "MAR: {:.2f}".format(mar), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
-        # Draw text if mouth is open
-		if mar > MOUTH_AR_THRESH:
-			cv2.putText(frame, "Mouth is Open!", (30,60),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255),2)
-	# Write the frame into the file 'output.avi'
-	out.write(frame)
-	# show the frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+	# Draw text if mouth is open
+	if mar > MOUTH_AR_THRESH:
+		is_talk_in_frame = True
+print(f"Is talk in frame: {is_talk_in_frame}")
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
-vs.stop()
+
